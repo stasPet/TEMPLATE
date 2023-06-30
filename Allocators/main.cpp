7c290b5d8eca55ea6865d9bbb9e572d41ce56b5d
Stanislav <petrov_stas@outlook.com>
2023-06-30 14:16:13 +0300

    add all files

--- Allocators/main.cpp
+++ Allocators/main.cpp
@@ -1,4 +1,23 @@
+#include <vector>
+#include <string>
+
+#include "Alexandrescu/Allocator.h"
+
 int main()
 {
+  {
+  std::vector< std::string, Loki::LokiAllocator< std::string > > vector;
 
+  for ( unsigned i{}; i < 1000 * 1000 * 5; ++i )
+    vector.push_back( "Hello World!" );
+  }
+
+ {
+  std::vector< std::string, Loki::LokiAllocator< std::string > > vector;
+
+  for ( unsigned i{}; i < 1000 * 1000 * 5; ++i )
+    vector.push_back( "Hello World!" );
+  }
+
+  return 0;
 }
