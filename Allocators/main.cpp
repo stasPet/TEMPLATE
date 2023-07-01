6397321a0f0d90f1d1bf390e31176135e6451d26
Stanislav <petrov_stas@outlook.com>
2023-07-01 21:05:12 +0300

    replace auto_ptr, unique_ptr

--- Allocators/main.cpp
+++ Allocators/main.cpp
@@ -2,28 +2,50 @@
 #include <vector>
 #include <string>
 #include <thread>
+#include <chrono>
+#include <list>
 
 #include "Alexandrescu/Allocator.h"
+
+using namespace std::chrono_literals;
+
+struct TestStruct
+{
+  unsigned value;
+
+  TestStruct( unsigned i ) : value{ i }
+  {
+    std::cout << "TestStruct()" << value << '\n';
+    std::this_thread::sleep_for( 3s );
+  }
+
+  ~TestStruct()
+  {
+    std::cout << "~TestStruct()" << value << '\n';
+    std::this_thread::sleep_for( 3s );
+  }
+};
 
 void foo()
 {
   {
-    std::vector< std::string, Loki::LokiAllocator< std::string > > vector;
+    std::vector< TestStruct, Loki::LokiAllocator< TestStruct > > vector;
+    std::list< TestStruct, Loki::LokiAllocator< TestStruct > > list;
 
-    for ( unsigned i{}; i < 1000 * 1000 * 5; ++i )
-      vector.push_back( "Hello World!" );
+    for ( unsigned i{}; i < 10; ++i )
+    {
+      // vector.emplace_back( i );
+      list.emplace_back( i );
+    }
   }
 }
 
 int main()
 {
-  std::thread t1{ foo };
-  std::thread t2{ foo };
-
-  t1.detach();
-  t2.detach();
-
-  while ( true );
+  while ( true )
+  {
+    std::thread{ foo }.detach();
+  }
 
   return 0;
 }
