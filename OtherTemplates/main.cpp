19f6b8cb708d73c256f484e963e4402b51622804
Stanislav <petrov_stas@outlook.com>
2023-07-08 11:53:05 +0300

    test commit

--- OtherTemplates/main.cpp
+++ OtherTemplates/main.cpp
@@ -5,5 +5,5 @@ int main()
   BreadSlicer< Policy_3_is< DefaultPolicy_5 > > bs;
   bs.show();
 
-  return 1;
+  return 0;
 }
--- TypeList/main.cpp
+++ TypeList/main.cpp
@@ -5,5 +5,5 @@ using MyList = TypeList< bool, char, short, char[ 256 ], int, double >;
 int main()
 {
   LargestType< MyList >::Type t;
-  return 0;
+  return 1;
 }
